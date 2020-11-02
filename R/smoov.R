888cbf2704ab89f7eb6d2e361ea17b9589408973
Harvey Barnhard <hbarnhard@g.harvard.edu>
2020-11-02 15:55:41 -0500

    Smoov package initial commit

---
+++ R/smoov.R
@@ -0,0 +1,14 @@
+#' Simple mapping tool
+#'
+#' @param x
+#'
+#'
+#'
+#'
+smoov = function(x){
+  # Check if shapefile is loaded. If not then load and store in smoov
+  # environment
+  if(!exists(.smoov_env)){
+    .smoov_env = new.env()
+  }
+}
