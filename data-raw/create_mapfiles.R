9ea2a5c91b86ba70ad7f3e3cb2d750108184e57c
Harvey Barnhard <hbarnhard@g.harvard.edu>
2020-11-05 21:45:19 -0500

    Continued work on putting together many shapefiles

--- data-raw/create_mapfiles.R
+++ data-raw/create_mapfiles.R
@@ -2,13 +2,32 @@
 
 # Function to load tract maps
 load_merge_tracts = function(cb, year, class, outname){
-  colsTRUE2000 = c("AREA", "STATE", "COUNTY", "TRACT")
-  colsFALSE2000 = c("ALAND00", "STATEFP00", "COUNTYFP00", "TRACTCE00")
+  colsTRUE2000  = c("AREA", "STATE", "COUNTY", "TRACT")
+  colsFALSE2000 = c("ALAND00", "CTIDFP00")
+  colsTRUE2010  = c("CENSUSAREA", "STATE", "COUNTY", "TRACT")
+  colsFALSE2010 = c("ALAND10", "GEOID10")
+  newnames = c("area", "fips")
   for(i in 1:length(state.abb)){
     tractmap = tigris::tracts(state=state.abb[i],
                               cb=cb,
                               year=year,
                               class=class)[, get(paste0("cols", cb, year))]
+    # Concatenate local fips to create state-county-tract fips code
+    if(cb){
+      tractmap$fips = paste0(tractmap$STATE, tractmap$COUNTY, tractmap$TRACT)
+      if(year==2000){
+        tractmap = tractmap[, c("AREA", "fips")]
+      }else if(year==2010){
+        tractmap = tractmap[, c("CENSUSAREA", "fips")]
+      }
+    }
+    # Rename columns
+    if(class=="sf"){
+      colnames(tractmap) = newnames
+    }else if(class=="sp"){
+      colnames(tractmap@data) = newnames
+    }
+    # Save into global environment
     if(i==1){
       assign(outname, tractmap, envir=.GlobalEnv)
     }else{
@@ -17,7 +36,7 @@ load_merge_tracts = function(cb, year, class, outname){
   }
 }
 
-# FUnction to load county maps
+# Function to load county maps
 load_counties = function(cb, year, class, outname){
   countymap = tigris::counties(cb=coarse, year=yr, class=cl)
   assign(outname, countymap, envir=.GlobalEnv)
@@ -25,7 +44,8 @@ load_counties = function(cb, year, class, outname){
 
 # Load state abbreviations, adding DC
 data(state)
-state.abb = c(state.abb, "DC")
+# state.abb = c(state.abb, "DC")
+state.abb = "IN"
 
 # Initialize vector of names of mapfiles to save
 outnamevec = c()
@@ -48,30 +68,30 @@ for(geo in c("tracts", "counties")){
   }
 }
 
-#TODO figure out the best way to fortify for ggplot...this is slow
-
-in_tracts_sp = tigris::tracts("IN", year=2000, class="sp")
-in_tracts_sp@data$id = rownames(in_tracts_sp@data)
-in_tracts_sp = merge(x=fortify(in_tracts_sp), y=in_tracts_sp@data, by="id")
-in_tracts_sf = tigris::tracts("IN", year=2000, class="sf")
-
-random_data = data.table(in_tracts_sf[, c("CTIDFP00")])
-random_data[, geometry:=NULL] 
-random_data[, rando:= rnorm(nrow(random_data))]
-
-merge_fort_sf = function(x, y){
-  temp = merge(x, y, by="CTIDFP00")
-  ggplot(temp) +
-    geom_sf(aes(fill=rando))
-}
-merge_fort_sp = function(x, y){
-  temp = merge(x=x, y=y, by="CTIDFP00")
-  ggplot(temp, aes(x=long, y=lat, group=group, fill=rando)) +
-    geom_polygon()
-}
-
-microbenchmark::microbenchmark(
-  methodsf = merge_fort_sf(in_tracts_sf, random_data),
-  methodsp = merge_fort_sp(in_tracts_sp, random_data),
-  times=10
-)
+# #TODO figure out the best way to fortify for ggplot...this is slow
+# 
+# in_tracts_sp = tigris::tracts("IN", year=2000, class="sp")
+# in_tracts_sp@data$id = rownames(in_tracts_sp@data)
+# in_tracts_sp = merge(x=fortify(in_tracts_sp), y=in_tracts_sp@data, by="id")
+# in_tracts_sf = tigris::tracts("IN", year=2000, class="sf")
+# 
+# random_data = data.table(in_tracts_sf[, c("CTIDFP00")])
+# random_data[, geometry:=NULL] 
+# random_data[, rando:= rnorm(nrow(random_data))]
+# 
+# merge_fort_sf = function(x, y){
+#   temp = merge(x, y, by="CTIDFP00")
+#   ggplot(temp) +
+#     geom_sf(aes(fill=rando))
+# }
+# merge_fort_sp = function(x, y){
+#   temp = merge(x=x, y=y, by="CTIDFP00")
+#   ggplot(temp, aes(x=long, y=lat, group=group, fill=rando)) +
+#     geom_polygon()
+# }
+# 
+# microbenchmark::microbenchmark(
+#   methodsf = merge_fort_sf(in_tracts_sf, random_data),
+#   methodsp = merge_fort_sp(in_tracts_sp, random_data),
+#   times=10
+# )
