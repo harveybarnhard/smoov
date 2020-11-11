6f940014eefdb61ca972f2f9982de834b7423eee
Harvey Barnhard <hbarnhard@g.harvard.edu>
2020-11-11 10:31:32 -0500

    Cleaned up subsetting plot logic

--- R/smoov_plot.R
+++ R/smoov_plot.R
@@ -27,6 +27,7 @@ smoov_plot = function(geo,
   outlist = shp_data_merge(shp, data, id, value, states, tracts, counties)
   shp <- outlist[[1]]
   subset_logic = outlist[[2]]
+  
   # Move Hawaii and Alaska
   # https://www.r-spatial.org/r/2018/10/25/ggplot2-sf-3.html
   if(class=="sf"){
@@ -41,39 +42,65 @@ smoov_plot = function(geo,
                                      xlim=c(-161, -154),
                                      ylim=c(18, 23),
                                      expand=FALSE, datum=NA)
+    
+    # Determine whether to fill in value or just draw borders
     if(is.null(value) & is.null(data)){
-      g = ggplot2::ggplot(shp) + ggplot2::geom_sf()
-      if(subset_logic[1]){
-        g = g + usa_coord
-      }
-      if(subset_logic[2]){
-        alaska = ggplot2::ggplot(shp) +
-          ggplot2::geom_sf() +
-          alaska_coord +
-          ggplot2::theme_void()
-      }
-      if(subset_logic[3]){
-        hawaii = ggplot2::ggplot(shp) +
-          ggplot2::geom_sf() +
-          hawaii_coord +
-          ggplot2::theme_void()
-      }
+      fill_layer = ggplot2::geom_sf()
     }else{
-      g = ggplot2::ggplot(shp) +
-        ggplot2::geom_sf(aes(fill=value)) +
-        usa_coord
-      alaska = ggplot2::ggplot(shp[substr(shp$fips,1,2)=="02"]) +
-        ggplot2::geom_sf(aes(fill=value)) +
-        alaska_coord +
+      fill_layer = ggplot2::geom_sf(aes(fill=value))
+    }
+    
+    # Use different zoom levels depending on subsetting
+    # TODO: subset in shp_data_merge set for HI and AK
+    
+    # All US states
+    if(subset_logic[1]){
+      basemap = ggplot2::ggplot(shp) +
+        fill_layer +
+        usa_coord +
         ggplot2::theme_void()
-      hawaii = ggplot2::ggplot(shp[substr(shp$fips,1,2)=="15"]) +
-        ggplot2::geom_sf(aes(fill=value)) +
-        hawaii_coord +
+    }else{
+      basemap = ggplot2::ggplot(shp) +
+        fill_layer +
         ggplot2::theme_void()
     }
-    if(subset_logic[2] & subset_logic[3]){
+    
+    # Alaska
+    if(subset_logic[2]){
+      alaska = ggplot2::ggplot(shp) +
+        fill_layer +
+        alaska_coord +
+        ggplot2::theme_void()
+    }else if(subset_logic[1]){
+      alaska = ggplot2::ggplot() +
+        ggplot2::theme_void()
+    }
+    
+    # Hawaii
+    if(subset_logic[3]){
+      hawaii = ggplot2::ggplot(shp) +
+        fill_layer +
+        hawaii_coord +
+        ggplot2::theme_void()
+    }else if(subset_logic[1]){
+      hawaii = ggplot2::ggplot() +
+        ggplot2::theme_void()
+    }
+    
+    # Return ggplot2 object based on subset logic
+    #    1. If just HI and AK are plotted (TODO)
+    #    2. If just AK is plotted
+    #    3. If just HI is plotted
+    if(!subset_logic[1] & subset_logic[2] & !subset_logic[3]){
+      return(alaska)
+    }
+    if(!subset_logic[1] & !subset_logic[2] & subset_logic[3]){
+      return(hawaii)
+    }
+    
+    if(subset_logic[1]){
       return(
-        g +
+        basemap + 
           ggplot2::annotation_custom(
             grob = ggplot2::ggplotGrob(alaska),
             xmin = -2750000,
@@ -90,17 +117,8 @@ smoov_plot = function(geo,
           ) +
           ggplot2::theme_void()
       )
-    }else if(subset_logic[2]){
-      return(
-        alaska
-      )
-    }
-    else if(subset_logic[3]){
-      return(
-        hawaii
-      )
     }else{
-      return(g + ggplot2::theme_void())
+      return(basemap)
     }
   }else if(class=="sp"){
     if(length(value)==0 & is.null(data)){
